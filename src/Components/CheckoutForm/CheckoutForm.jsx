13634d0f7587a13ae0a4a8874b8511f1f0818c57
Ahbabuzzaman <160356472+ahbab-zaman@users.noreply.github.com>
2025-01-20 02:35:42 +0600

    finish payment using stripe

--- src/Components/CheckoutForm/CheckoutForm.jsx
+++ src/Components/CheckoutForm/CheckoutForm.jsx
@@ -3,16 +3,22 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import useAxiosSecure from "../../Hooks/useAxiosSecure";
 import useCart from "../../Hooks/useCart";
+import useAuth from "../../Hooks/useAuth";
+import { toast, Zoom } from "react-toastify";
+// import Moment from "react-moment";
 
 const CheckoutForm = () => {
   const stripe = useStripe();
   const elements = useElements();
+  const { user } = useAuth();
   const [clientSecret, setClientSecret] = useState("");
+  const [error, setError] = useState("");
   const navigate = useNavigate();
   const axiosSecure = useAxiosSecure();
   const [cart, refetch] = useCart();
   const totalPrice = cart.reduce((prev, curr) => prev + curr.price, 0);
-  console.log(totalPrice);
+  //   const now = new Date();
+  //   const date = <Moment>{now}</Moment>;
   useEffect(() => {
     if (totalPrice > 0) {
       axiosSecure
@@ -22,7 +28,7 @@ const CheckoutForm = () => {
           setClientSecret(res.data.clientSecret);
         });
     }
-  }, []);
+  }, [axiosSecure, totalPrice]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -41,8 +47,10 @@ const CheckoutForm = () => {
 
     if (error) {
       console.log(error);
+      setError(error.message);
     } else {
       console.log("Payment Method", paymentMethod);
+      setError("");
     }
 
     const { paymentIntent, err } = await stripe.confirmCardPayment(
@@ -51,8 +59,8 @@ const CheckoutForm = () => {
         payment_method: {
           card: card,
           billing_details: {
-            name: user?.displayName,
-            email: user?.email,
+            name: user?.displayName || "Unknown User",
+            email: user?.email || "Unknown User",
           },
         },
       }
@@ -77,8 +85,21 @@ const CheckoutForm = () => {
         };
         const res = await axiosSecure.post("/payments", payment);
         refetch();
-        navigate("/dashboard/payHistory");
+        // navigate("/");
         console.log(res.data);
+        if (res.data.paymentResult?.insertedId) {
+          toast("💸 Payment Successful", {
+            position: "top-right",
+            autoClose: 700,
+            hideProgressBar: false,
+            closeOnClick: false,
+            pauseOnHover: true,
+            draggable: false,
+            progress: undefined,
+            theme: "light",
+            transition: Zoom,
+          });
+        }
       }
     }
   };
@@ -108,6 +129,7 @@ const CheckoutForm = () => {
         >
           Pay
         </button>
+        <p className="text-red-600">{error}</p>
       </form>
     </div>
   );
